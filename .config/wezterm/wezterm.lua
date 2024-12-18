b314a00da4601a88ef02b0a81851d4b4c200404b
Aditya Dhara <dev@adityadhara.com>
2024-12-18 00:08:31 -0800

    Some more tweaks

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,8 +4,10 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.font_size = 12
+config.font_size = 13
 config.color_scheme = "Tokyo Night"
+
+config.tab_bar_at_bottom = true
 
 -- and finally, return the configuration to wezterm
 return config
