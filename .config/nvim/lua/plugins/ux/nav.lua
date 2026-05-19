6076b228bd9de02cd8b041a9a20d5dfcf0345c22
Aditya Dhara <dev@adityadhara.com>
2026-05-18 23:47:52 -0700

    Stuff

--- .config/nvim/lua/plugins/ux/nav.lua
+++ .config/nvim/lua/plugins/ux/nav.lua
@@ -68,16 +68,6 @@ return {
       }
     },
   },
-  {
-    "antosha417/nvim-lsp-file-operations",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-neo-tree/neo-tree.nvim", -- makes sure that this loads after Neo-tree.
-    },
-    config = function()
-      require("lsp-file-operations").setup()
-    end,
-  },
 
   {
     "error311/wayfinder.nvim",
--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -5,12 +5,14 @@ vim.opt.updatetime = 300
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
 
+-- spacing
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.softtabstop = 2
 vim.opt.fillchars = { eob = " " }
 
+-- keymaps
 vim.keymap.set("i", "jk", "<ESC>", { silent = true })
 vim.keymap.set("n", "<leader>c", ":bd<CR>", { silent = true })
 vim.keymap.set("n", "<leader>?", function() require("key_guide").start("n") end, { desc = "Key guide" })
@@ -20,4 +22,8 @@ vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- wrap and fold
 vim.opt.wrap = false
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
